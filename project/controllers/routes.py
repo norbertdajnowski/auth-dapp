b25cfc6bdc7a7784cc2553f4214cdb5f9e15a91e
norbertdajnowski <norbertdajnowski@gmail.com>
2023-07-20 15:01:48 +0100

    Auth web app merge

---
+++ project/controllers/routes.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from project import app
+from flask import render_template, request
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+
+from project.models.voice import voice
+
+voiceObj = voice()
+
+class CreateForm(FlaskForm):
+    text = StringField('name', validators=[DataRequired()])
+
+
+@app.route('/')
+def start():
+    return render_template('printer/index.html')
+
+@app.route('/about')
+def about():
+    return render_template('printer/about.html')
+
+@app.route('/login')
+def login():
+    return render_template('printer/login.html')
+
+@app.route('/register')
+def register():
+    return render_template('printer/register.html')
+
+@app.route('/delete')
+def delete():
+    return render_template('printer/delete.html')
+
+@app.route('/addVoice', methods=['GET', 'POST'])
+def addVoice():
+    voice1 = request.files['voice1'].read()
+    voice2 = request.files['voice2'].read()
+    voice3 = request.files['voice3'].read()
+    username = request.files['username'].read().decode("utf-8") 
+
+    voiceObj.add_user(voice1, voice2, voice3, username)
+
+    return render_template('printer/index.html')
+
+
+@app.route('/recognise', methods=['GET', 'POST'])
+def recognise():
+    voice = request.files['voice1'].read()
+    username = request.files['username'].read().decode("utf-8") 
+    loginResult = voiceObj.recognise(voice, username)
+    print("login result - " + loginResult)
+    if loginResult == "Not Found" or loginResult == None:
+        return f'We could not identify your account, refresh and try again.'
+    elif loginResult == "Username Missing":
+        return f'Your username does not exist in the database'
+    else:
+        return f'Identification completed succesfully!'
+
+
+@app.route('/deleteVoice', methods=['GET', 'POST'])
+def deleteVoice():
+    username = request.files['username'].read().decode("utf-8") 
+    deleteResult = voiceObj.delete_user(username)
+    print("Delete user result - " + deleteResult)
+    if deleteResult == "deleted":
+        return f'User deleted succesfully'
+    elif deleteResult == "No user":
+        return f'Username was not found in our database'
+    else:
+        return f'Problem encountered during user deletion'
+    
---
+++ project/models/voice.py
@@ -0,0 +1,159 @@
+# -*- coding: utf-8 -*-
+from flask import flash
+import pyaudio
+import wave
+import cv2
+import os
+import glob
+import pickle
+import time
+import io
+from scipy.io.wavfile import read
+from IPython.display import Audio, display, clear_output
+
+from project.models.main_functions import *
+
+class voice(object):
+
+    def __init__(self) -> None:
+        self.FORMAT = pyaudio.paInt16
+        self.CHANNELS = 2
+        self.RATE = 48000
+        self.CHUNK = 1024
+        self.RECORD_SECONDS = 3
+
+        self.FILENAME = "./test.wav"
+        self.MODEL = "\\gmm_models\\voice_auth.gmm"
+        self.VOICEPATH = "\\voice_database\\"
+
+        self.VOICEDICT = {}
+
+    def add_user(self, voice1, voice2, voice3, username):   
+
+        source = self.VOICEPATH + username
+        absolute_path = os.path.dirname(__file__) + self.VOICEPATH + username
+        os.mkdir(absolute_path)
+        
+        voice_dir = [voice1, voice2, voice3]
+        self.VOICEDICT[username] = voice_dir
+        X = []
+        Y = []
+        for name in voice_dir:
+            # reading audio files of speaker
+            (sr, audio) = read(io.BytesIO(name))
+                    
+            # extract 40 dimensional MFCC
+            vector = extract_features(audio,sr)
+            vector = vector.flatten()
+            X.append(vector)
+            Y.append(name)
+        X = np.array(X, dtype=object)
+
+        le = preprocessing.LabelEncoder()
+        le.fit(Y)
+        Y_trans = le.transform(Y)
+        clf = LogisticRegression(random_state=0).fit(X.tolist(), Y_trans)
+
+
+        if os.path.isfile(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm"): 
+            os.remove(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm")
+        # saving model
+        pickle.dump(clf, open(os.path.dirname(__file__) + '\\gmm_models\\voice_auth.gmm', 'wb'))
+        print(username + ' added successfully') 
+        
+        features = np.asarray(())
+
+    def recognise(self, voice, username):
+        # Voice Authentication
+        VOICENAMES = [ name for name in os.listdir(os.path.dirname(__file__) + self.VOICEPATH) if os.path.isdir(os.path.join(os.path.dirname(__file__) + self.VOICEPATH, name)) ]
+        print(VOICENAMES)
+
+        if username in VOICENAMES:
+            userIndex = VOICENAMES.index(username)
+            arr = [VOICENAMES[userIndex]]
+            try:
+                # load model 
+                model = pickle.load(open(os.path.dirname(__file__) + self.MODEL,'rb'))
+
+                # reading audio files of speaker
+                (sr, audio) = read(io.BytesIO(voice))
+                    
+                # extract 40 dimensional MFCC
+                vector = extract_features(audio,sr)
+                vector = vector.flatten()
+                test_audio = vector
+
+                # predict with model
+                pred = model.predict(test_audio.reshape(1,-1))
+
+                # decode predictions
+                le = preprocessing.LabelEncoder()
+                le.fit(arr)
+                identity = le.inverse_transform(pred)[0]
+
+                # if voice not recognized than terminate the process
+                if identity == 'unknown':
+                    print("Not Recognised!")
+                    return "Not Found"
+                else:
+                    print( "Recognized as - ", username)
+                    return "Identified and logged in!"
+                    
+            except:
+                print("Stopped")
+                return "Not Found"
+        else:
+            return "Username Missing"
+
+    def delete_user(self, username):
+
+        try:
+            name = username 
+
+            users = [ name for name in os.listdir(os.path.dirname(__file__) + self.VOICEPATH) if os.path.isdir(os.path.join(os.path.dirname(__file__) + self.VOICEPATH, name)) ]
+            
+            if name not in users or name == "unknown":
+                print('No such user !!')
+                return "No user"
+
+            [os.remove(path) for path in glob.glob(os.path.dirname(__file__) + self.VOICEPATH + name + '/*')]
+            os.removedirs(os.path.dirname(__file__) + self.VOICEPATH + name)
+
+            if os.path.isfile(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm"): 
+                os.remove(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm")
+                
+            voice_dir = [ name for name in os.listdir(os.path.dirname(__file__) + self.VOICEPATH) if os.path.isdir(os.path.join(os.path.dirname(__file__) + self.VOICEPATH, name)) ]
+            X = []
+            Y = []
+            for voice in self.VOICEDICT[username]:
+                # reading audio files of speaker
+                print("test")
+                (sr, audio) = read(io.BytesIO(voice))
+                            
+                # extract 40 dimensional MFCC
+                vector = extract_features(audio,sr)
+                vector = vector.flatten()
+                X.append(vector)
+                Y.append(name)
+                
+            X = np.array(X, dtype=object)
+
+            le = preprocessing.LabelEncoder()
+            le.fit(Y)
+            Y_trans = le.transform(Y)
+            clf = LogisticRegression(random_state=0).fit(X.tolist(), Y_trans)
+
+            if os.path.isfile(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm"): 
+                os.remove(os.path.dirname(__file__) + "\\gmm_models\\voice_auth.gmm")
+            # saving model
+            pickle.dump(clf, open(os.path.dirname(__file__) + '\\gmm_models\\voice_auth.gmm', 'wb'))
+
+            print('User ' + name + ' deleted successfully')
+            return "deleted"
+
+        except:
+            print("Error encountered")
+            return "deleted"
+
+
+
